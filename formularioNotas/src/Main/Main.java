571c68a011f50305101babf852107f98f726f670
lazopachecomiguelangel@gmail.com <lazopachecomiguelangel@gmail.com>
2015-09-16 08:24:41 -0400

    hhyuu

--- formularioNotas/src/Main/Main.java
+++ formularioNotas/src/Main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Se Inicia el programa desde aqui er
-		//lkjhasfkd gadf
+		//lkjhasfkd gadf hyuuh
 		Login window = new Login();
 		window.getFrame().setVisible(true);
 
