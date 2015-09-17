e75dda0684a24b0b58e53ddff464e38c1e85cbe3
Chukaro <lordchukaro@gmail.com>
2015-09-17 12:42:30 -0400

    se modifico presntacion>inicio

--- formularioNotas/src/Presentation/Inicio.java
+++ formularioNotas/src/Presentation/Inicio.java
@@ -15,6 +15,8 @@ import javax.swing.JTabbedPane;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import javax.swing.JComboBox;
 
 public class Inicio {
 
@@ -56,14 +58,32 @@ public class Inicio {
 		panel.add(btnNewButton_1);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(null, "Busqeuda Formulasio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Busqueda Formulario", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_1.setBounds(10, 106, 414, 58);
 		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(10, 27, 119, 20);
+		panel_1.add(comboBox);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(165, 27, 67, 20);
+		panel_1.add(comboBox_1);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "Materia", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_2.setBounds(10, 175, 414, 55);
 		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setBounds(10, 24, 117, 20);
+		panel_2.add(comboBox_2);
+		
+		JComboBox comboBox_3 = new JComboBox();
+		comboBox_3.setBounds(167, 24, 65, 20);
+		panel_2.add(comboBox_3);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
@@ -80,7 +100,16 @@ public class Inicio {
 		JMenu mnBuscarEstudiante = new JMenu("Buscar Estudiante");
 		menuBar.add(mnBuscarEstudiante);
 		
-		JMenu mnCuente = new JMenu("Cuente");
+		JMenu mnInformes = new JMenu("Informes");
+		menuBar.add(mnInformes);
+		
+		JMenuItem mntmAprobados = new JMenuItem("Aprobados");
+		mnInformes.add(mntmAprobados);
+		
+		JMenuItem mntmReprobados = new JMenuItem("Reprobados");
+		mnInformes.add(mntmReprobados);
+		
+		JMenu mnCuente = new JMenu("Cuenta");
 		menuBar.add(mnCuente);
 		
 		JMenuItem mntmCambiarContrasea = new JMenuItem("Cambiar contrase\u00F1a");
