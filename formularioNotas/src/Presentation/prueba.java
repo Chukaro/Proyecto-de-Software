6d9f111410a3e63aa23a4de6d31177a1aba79285
Chukaro <lordchukaro@gmail.com>
2015-09-19 17:24:35 -0400

    prueba tabla

---
+++ formularioNotas/src/Presentation/prueba.java
@@ -0,0 +1,50 @@
+package Presentation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+
+public class prueba {
+
+	private JFrame frame;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					prueba window = new prueba();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public prueba() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.getContentPane().setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(181, 68, -81, 76);
+		frame.getContentPane().add(table);
+	}
+
+}
